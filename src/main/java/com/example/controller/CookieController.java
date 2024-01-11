5306da22daf48b0943a0db67178b6ddc29b04718
s19a <154731769+s19a@users.noreply.github.com>
2024-01-11 16:12:42 +0900

    テストcommit2

--- src/main/java/com/example/controller/CookieController.java
+++ src/main/java/com/example/controller/CookieController.java
@@ -3,7 +3,6 @@ package com.example.controller;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
-//テスト
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +15,6 @@ public class CookieController {
 	@GetMapping("/set")
     @ResponseBody
     public String setCookie(@RequestParam String value, HttpServletResponse res) {
-        // cookie名をtest_cookieとして保存します
         res.addCookie(new Cookie("test_cookie", value));
         return "保存しました";
     }
